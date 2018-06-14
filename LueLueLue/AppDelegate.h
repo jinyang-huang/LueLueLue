ca6ab675ad520f50c106ef6cabed815b7d1b311e
hjy <inc@incdewin10.local>
2018-06-14 10:14:52 +0800

    Initial Commit

---
+++ LueLueLue/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  LueLueLue
+//
+//  Created by inception on 2018/6/14.
+//  Copyright © 2018年 hjy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ LueLueLue/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LueLueLue
+//
+//  Created by inception on 2018/6/14.
+//  Copyright © 2018年 hjy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
